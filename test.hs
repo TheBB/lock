30b82108230899586285fdb3b09d84bc36edd4b7
Eivind Fonn <evfonn@gmail.com>
2013-09-16 23:01:55 +0200

    File encryption working.

--- test.hs
+++ test.hs
@@ -22,17 +22,19 @@ import Data.Char (
                  , chr
                  )
 import Data.LargeWord (Word128)
+import Data.List.Split (chunksOf)
 import Data.Word (Word8)
+import Numeric (showHex)
 -- }}}
 
 key = 0x06a9214036b8a15b512e03d534120006 :: Word128
 iv  = 0x3dafba429d9eb430b422da802c9fac41 :: Word128
 
-{-plaintext = "This is a string with some vaguely specified length."-}
+plaintext = "This is a string with some vaguely specified length."
 
-{-ciphertext = cbc encrypt iv key $ pkcs5 $ map (fromIntegral . ord) plaintext-}
+ciphertext = cbc encrypt iv key $ pkcs5 $ map (fromIntegral . ord) plaintext
 
-{-plaintext' = map (chr . fromIntegral) $ unPkcs5 $ unCbc decrypt iv key ciphertext-}
+plaintext' = map (chr . fromIntegral) $ unPkcs5 $ unCbc decrypt iv key ciphertext
 
 loadPlain :: FilePath -> IO [Word8]
 loadPlain fn = do
@@ -46,12 +48,12 @@ writePlain fn bytes = do
 loadEncrypted :: FilePath -> IO [Word8]
 loadEncrypted fn = do
     ciphertext <- loadPlain fn
-    return $ unPkcs5 $ unCbc decrypt iv key (pkcs5 ciphertext)
+    return $ unPkcs5 $ unCbc decrypt iv key (bytesToWords ciphertext)
 
 writeEncrypted :: FilePath -> [Word8] -> IO ()
 writeEncrypted fn bytes = do
     let ciphertext = cbc encrypt iv key (pkcs5 bytes)
-    writePlain fn (unPkcs5 ciphertext)
+    writePlain fn (wordsToBytes ciphertext)
 
 dostuff :: IO ()
 dostuff = do
@@ -63,3 +65,26 @@ redostuff = do
     bytes <- loadEncrypted "test-enc.txt"
     writePlain "test-dec.txt" bytes
 
+wordToString :: Word128 -> String
+wordToString w = replicate (32 - length s) '0' ++ s
+    where s = showHex (fromIntegral w) ""
+
+stringToWord :: String -> Word128
+stringToWord s = fromIntegral (read ("0x" ++ s))
+
+stringToBytes :: String -> [Word8]
+stringToBytes "" = []
+stringToBytes [a] = []
+stringToBytes (a:b:ss) = (read ("0x" ++ [a,b])) : stringToBytes ss
+
+byteToString :: Word8 -> String
+byteToString b = replicate (2 - length s) '0' ++ s
+    where s = showHex b ""
+
+bytesToString :: [Word8] -> String
+bytesToString = concat . (map byteToString)
+
+wordsToBytes = concat . map (stringToBytes . wordToString)
+
+bytesToWords :: [Word8] -> [Word128]
+bytesToWords bs = map (stringToWord . bytesToString) $ chunksOf 16 bs
