8497e1f9277c11d5a8f5e07668e75b490cdd9d83
Eivind Fonn <evfonn@gmail.com>
2013-09-18 20:25:57 +0200

    Time.

--- lock.hs
+++ lock.hs
@@ -21,20 +21,27 @@ import Data.Char (
                    ord
                  , chr
                  )
+import Data.Maybe (isNothing)
 import Data.LargeWord (Word128)
 import Data.List.Split (chunksOf)
 import Data.Word (Word8)
+import Network.HTTP ( 
+                      getResponseBody
+                    , getRequest
+                    , simpleHTTP
+                    )
 import Numeric (showHex)
+import Text.Regex (
+                    matchRegex
+                  , mkRegex
+                  )
 -- }}}
+
+-- {{{ Encryption
 
 key = 0x06a9214036b8a15b512e03d534120006 :: Word128
 iv  = 0x3dafba429d9eb430b422da802c9fac41 :: Word128
 
-plaintext = "This is a string with some vaguely specified length."
-
-ciphertext = cbc encrypt iv key $ pkcs5 $ map (fromIntegral . ord) plaintext
-
-plaintext' = map (chr . fromIntegral) $ unPkcs5 $ unCbc decrypt iv key ciphertext
 
 loadPlain :: FilePath -> IO [Word8]
 loadPlain fn = do
@@ -88,3 +95,25 @@ wordsToBytes = concat . map (stringToBytes . wordToString)
 
 bytesToWords :: [Word8] -> [Word128]
 bytesToWords bs = map (stringToWord . bytesToString) $ chunksOf 16 bs
+
+-- }}}
+
+-- {{{ Time
+getTimeSite :: String -> String -> IO (Maybe Integer)
+getTimeSite uri regex = do
+    rsp <- simpleHTTP (getRequest uri)
+    txt <- getResponseBody rsp
+    let matches = map (matchRegex (mkRegex regex)) $ lines txt
+    case filter (not . isNothing) matches of
+        ((Just m):_) -> return $ Just ((read . head) m :: Integer)
+        [] -> return Nothing
+
+getTime :: IO (Maybe Integer)
+getTime = do
+    cts <- getTimeCTS
+    case cts of
+        Just a -> return $ Just a
+        Nothing -> getTimeUTS
+    where getTimeCTS = getTimeSite "http://www.currenttimestamp.com/" "\\s*current_time\\s*=\\s*([0-9]+);\\s*"
+          getTimeUTS = getTimeSite "http://www.unixtimestamp.com/" "\\s*([0-9]+)\\s*UTC"
+-- }}}
