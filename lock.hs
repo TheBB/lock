632b7981bb910d5ef19e35feced1d615116f3fb9
Eivind Fonn <evfonn@gmail.com>
2013-10-10 14:59:18 +0200

    I think MaybeT works fine like this.

--- lock.hs
+++ lock.hs
@@ -2,9 +2,11 @@
 import Codec.Encryption.AES (decrypt, encrypt)
 import Codec.Encryption.Modes (cbc, unCbc)
 import Codec.Encryption.Padding (pkcs5, unPkcs5)
+import Control.Monad.Maybe (MaybeT, runMaybeT)
 import Control.Monad.Trans (lift)
 import Data.Bits ((.|.), shiftL, shiftR)
 import Data.ByteString as BS (pack, readFile, unpack, writeFile)
+import Data.Char (isDigit, digitToInt)
 import Data.Maybe (isNothing)
 import Data.LargeWord (Word128)
 import Data.List (isPrefixOf)
@@ -33,6 +35,8 @@ import System.Console.Haskeline.Completion (completeWord, simpleCompletion, Comp
 import qualified System.Exit (exitSuccess, exitFailure)
 import Text.Regex (matchRegex, mkRegex)
 -- }}}
+
+version = "lock version 0.0.1"
 
 -- {{{ Encryption
 key = 0x06a9214036b8a15b512e03d534120006 :: Word128
@@ -101,20 +105,45 @@ getTime = do
         Nothing -> getTimeUTS
     where getTimeCTS = getTimeSite "http://www.currenttimestamp.com/" "\\s*current_time\\s*=\\s*([0-9]+);\\s*"
           getTimeUTS = getTimeSite "http://www.unixtimestamp.com/" "\\s*([0-9]+)\\s*UTC"
+
+timeMults = [('s', 1), ('m', 60), ('h', 60*60), ('d', 24*60*60), ('w', 7*24*60*60)]
+parseTime :: String -> Maybe Integer
+parseTime = parseAcc 0 0
+    where parseAcc glob loc (c:cs)
+              | isDigit c = parseAcc glob (10*loc + fromIntegral (digitToInt c)) cs
+              | c `elem` "smhdw" = let Just mul = lookup c timeMults
+                              in parseAcc (glob + mul*loc) 0 cs
+              | otherwise = Nothing
+          parseAcc glob loc [] = Just (glob + loc)
+
+maybeReadTime :: String -> MaybeIO Integer
+maybeReadTime prompt = do
+    inp <- maybeRun (getInputLine prompt)
+    let time = parseTime inp
+    case time of
+        Nothing -> fail "Unable to parse time"
+        Just t -> return t
 -- }}}
 
 -- {{{ Lock status
-data Status = Status { timeStarted :: Integer }
+data Status = Status {
+                       timeStarted :: Integer 
+                     , timeUnlock :: Integer
+                     }
     deriving (Read, Show)
 -- }}}
 
+-- {{{ Monad transformers
+type InputIO = InputT IO
+type MaybeIO = MaybeT InputIO
+-- }}}
+
 -- {{{ Start new session
-startNewSession :: FilePath -> InputT IO ()
+startNewSession :: String -> MaybeIO Status
 startNewSession fp = do
-    inp <- getInputLine "Initial time to release: "
-    case inp of
-        Nothing -> return ()
-        Just s -> outputStrLn s
+    initTime <- maybeRun (lift getTime)
+    initLen <- maybeReadTime "Initial time to unlock: "
+    return (Status initTime (initTime + initLen))
 -- }}}
 
 -- {{{ Main option processing and Haskeline settings
@@ -135,17 +164,28 @@ searchFunc :: String -> [Completion]
 searchFunc s = map simpleCompletion $ filter (s `isPrefixOf`) wordList
 hlSettings = setComplete (completeWord Nothing " \t" $ return . searchFunc) defaultSettings
 
-exitSuccess = lift System.Exit.exitSuccess :: InputT IO a
-exitFailure = lift System.Exit.exitFailure :: InputT IO a
+maybeRun :: InputIO (Maybe a) -> MaybeIO a
+maybeRun act = do
+    res <- lift act
+    case res of
+        Nothing -> fail ""
+        Just s -> return s
 
-processFlags :: [Flag] -> InputT IO ()
+exitSuccess = lift System.Exit.exitSuccess :: InputIO a
+exitFailure = lift System.Exit.exitFailure :: InputIO a
+
+processFlags :: [Flag] -> InputIO ()
 processFlags [] = return ()
 processFlags (flag:flags) = processFlag flag >> processFlags flags
 
-processFlag :: Flag -> InputT IO ()
-processFlag Version = outputStrLn "v 0.1" >> exitSuccess
+processFlag :: Flag -> InputIO ()
+processFlag Version = outputStrLn version >> exitSuccess
 processFlag Help = outputStrLn usage >> exitSuccess
-processFlag (Image fp) = startNewSession fp >> exitSuccess
+processFlag (Image fp) = do
+    status <- runMaybeT (startNewSession fp)
+    case status of
+        Nothing -> outputStrLn "Unable to start session" >> exitFailure
+        Just st -> outputStrLn (show st) >> exitSuccess
 processFlag (Session fp) = outputStrLn ("Session: " ++ fp) >> exitSuccess
 
 main :: IO ()
